6d644daef293b3366e132eaabb58e8862241f381
Ngo Nhan <ngonhan2k5@gmail.com>
2020-08-08 23:31:50 -0500

    Order model

--- backend-app/routes/orders.js
+++ backend-app/routes/orders.js
@@ -9,4 +9,31 @@ router.get('/:orderCode', function (req, res, next) {
   })
 });
 
+
+router.get('/:status', function (req, res, next) {
+  const { status='*' } = req.params
+  const query = status=='*'?{}:{status};
+  Order.find({ query }, (err, orders) => {
+    res.json(orders)
+  })
+});
+
+
+router.post('/', function(req, res, next) {
+  const { customerId, farmerId, productIds} = body;
+  Order.$$.createOrder(customerId, farmerId, productIds).then(
+    (result) => res.json(result)
+  );
+  
+
+});
+
+router.put('/:id', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.delete('/:id', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
 module.exports = router;
