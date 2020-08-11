01036cef1acaea2b2993c78fe5eb78a48e89380e
knguyen93 <khanhnt.tdt@gmail.com>
2020-08-11 16:21:31 -0500

    re-organize code

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: 'farmers', loadChildren: () => import('./farmers/farmers.module').then(m => m.FarmersModule) },
   { path: 'customers', loadChildren: () => import('./customers/customers.module').then(m => m.CustomersModule) },
-  { path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) }
+  { path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) },
+  { path: 'orders', loadChildren: () => import('./orders/orders.module').then(m => m.OrdersModule) }
 ];
 
 @NgModule({
@@ -12,3 +13,20 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+/**
+ * /farmers => Customer
+ * /farmers/:id
+ * /farmers/:id/products
+ * /farmers/:id/products/:prodId
+ * /customers/shopping-cart
+ * /customers/checkout
+ * /customers/orders
+ *
+ * /products => Farmer (manage products)
+ * /products/add
+ * /products/:id/update
+ * /orders -> manages Order status
+ * /orders/:id
+ *
+ */
